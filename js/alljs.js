b13be7b1a6f05c90810ea2a1c471dadf7d73e50f
d93fu6513 <102431864+d93fu6513@users.noreply.github.com>
2022-04-11 00:13:34 +0800

    back top

--- js/alljs.js
+++ js/alljs.js
@@ -16,6 +16,17 @@ $(document).ready(function () {
     },
   });
 
+// 捲軸偵測距離頂部超過 50 才顯示按鈕
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 50) {
+    if ($(".back-top").hasClass("hide")) {
+      $(".back-top").toggleClass("hide");
+    }
+  } else {
+    $(".back-top").addClass("hide");
+  }
+});
+
   // Example starter JavaScript for disabling form submissions if there are invalid fields
   (function () {
     'use strict'
